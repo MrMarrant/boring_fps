a6ddc3f99ad80a2d775a42881cb995b389e0db35
MrMarrant <laughymclaugher@gmail.com>
2025-10-12 10:14:03 +0200

    [UPDATE] Error message fetch patch

--- gamemodes/boringfps/gamemode/cl_init.lua
+++ gamemodes/boringfps/gamemode/cl_init.lua
@@ -2,6 +2,6 @@ include( "shared.lua" )
 
 hook.Add("OnGameLoaded", "BoringFPS:OnGameLoadedClient", function()
     BoringFPS.FetchData(BoringFPS_CONFIG.Links.PatchNote, function(success, data, code)
-        BoringFPS_CONFIG.Vars.LastPatch = success and data or ""
+        BoringFPS_CONFIG.Vars.LastPatch = success and data or "ERROR FETCHING PATCH NOTE"
     end)
 end)
